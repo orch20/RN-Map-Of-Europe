a639ca3d87731e65c2dff0d35facef72f8a61321
Orest Chornyi <orest.chorny@gmail.com>
2025-11-04 12:30:13 +0200

    Add interactive map component and initial map screen layout
    - Implemented MapOfEurope component with pan gesture support for interactive map navigation.
    - Created MapScreen component with basic layout and status bar integration.

--- App.js
+++ App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import MapScreen from "./src/components/MapScreen";
+import MapOfEurope from "./src/components/MapOfEurope";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      {/* <MapScreen /> */}
+      <MapOfEurope />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ src/components/MapOfEurope.jsx
@@ -0,0 +1,63 @@
+import React, { useRef } from 'react';
+
+import { Animated, PanResponder, StyleSheet, View } from 'react-native';
+import EuropeMapSvg from '../../assets/EuropeMapSvg';
+
+
+const MapOfEurope = () => {
+  const translation = useRef(new Animated.ValueXY({ x: 0, y: 0 })).current;
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: (_, gestureState) => {
+        const { dx, dy } = gestureState;
+        return Math.abs(dx) > 2 || Math.abs(dy) > 2;
+      },
+      onPanResponderGrant: () => {
+        translation.stopAnimation((value) => {
+          translation.setOffset({ x: value.x, y: value.y });
+          translation.setValue({ x: 0, y: 0 });
+        });
+      },
+      onPanResponderMove: Animated.event(
+        [null, { dx: translation.x, dy: translation.y }],
+        { useNativeDriver: false },
+      ),
+      onPanResponderRelease: () => {
+        translation.flattenOffset();
+      },
+      onPanResponderTerminate: () => {
+        translation.flattenOffset();
+      },
+    }),
+  ).current;
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        {...panResponder.panHandlers}
+        style={[styles.mapWrapper, { transform: translation.getTranslateTransform() }]}
+      >
+        <EuropeMapSvg style={styles.map} />
+      </Animated.View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    overflow: 'hidden',
+    backgroundColor: '#fff',
+  },
+  mapWrapper: {
+    width: '180%',
+    height: '180%',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
+
+export default MapOfEurope;
